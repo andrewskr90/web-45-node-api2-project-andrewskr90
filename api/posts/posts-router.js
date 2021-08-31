bdd2b06e2e04b3a267207d71bcb099a5d2bbfe99
andrewskr90 <andrewskr90@gmail.com>
2021-08-31 16:12:21 -0700

    trouble responding with added post and edited post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -30,4 +30,60 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const post = req.body
+    console.log(post)
+    if (!post.title || !post.contents) {
+        res.status(400)
+            .json({ 
+                message: "Please provide title and contents for the post"
+            })
+    } else {
+        Post.insert(post)
+            .then(id => {
+                // res.status(201)
+                //     .json(post)
+                Post.findById(id)
+                    .then(newPost => {
+                        res.status(201).json(newPost)
+                    })
+                    .catch(err => {
+                        console.log(err.message)
+                    })
+            })
+            .catch(err => {
+                res.status(500)
+                    .json({ 
+                        message: err.message 
+                    })
+            })
+        
+            
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const post = req.body
+    console.log(post)
+    if (!post.title || !post.contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+    }
+    Post.update(id, post)
+        .then(newPost => {
+            if (!newPost) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message
+            })
+        })
+})
+
+
+
 module.exports = router
